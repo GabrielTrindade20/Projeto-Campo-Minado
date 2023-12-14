37f6319dda236cba1b50b32261737b4cac8f8f8c
Gabriel <gabriel.rochat18@gmai.com>
2023-12-14 13:27:52 -0300

    criação componentes e minas

--- mines/App.js
+++ mines/App.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import params from './src/params';
-import Field from './src/componets/Field';
+import MineField from './src/componets/MineField';
+import {
+  createMineBoard
+} from './src/functions'
 
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  //Calcula a quantidade de minas no tabluleiro
+  minesAmount = () => {
+    const cols = params.getColumnsAmout()
+    const rows = params.getRowsamout()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  //Cria o estado do componente(ex: tabuleiro, os campos abertos, com mina)
+  createState = () => {
+    const cols = params.getColumnsAmout()
+    const rows = params.getRowsamout()
+    return {
+      board: createMineBoard(rows, cols, this.minesAmount())
+    }
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
@@ -14,35 +40,22 @@ export default class App extends Component {
           {params.getRowsamout()}x{params.getColumnsAmout()}
         </Text>
 
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1} />
-        <Field opened nearMines={2} />
-        <Field opened nearMines={3} />
-        <Field opened nearMines={6} />
-        <Field mined />
-        <Field mined opened/>
-        <Field mined opened exploded/>
-        <Field flagged/>
-        <Field flagged opened/>
-       
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
+
       </View>
     );
   }
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+  },
+  board: {
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#AAA'
   },
-  welcome: {
-    fontSize: 20,
-    margin: 10
-  },
-  instructions: {
-    fontSize: 15,
-    margin: 10,
-  }
 });
--- mines/src/componets/Field.js
+++ mines/src/componets/Field.js
@@ -43,7 +43,7 @@ export default props => {
             }
 
             {
-                mined && opened ? <Mine></Mine> : false
+                mined && opened ? <Mine /> : false
             }
 
             {
